d2c7094bb935a89a497407c92ee84384a86fe40f
Rangga Bayu Pratama <110972066+PowerGabb@users.noreply.github.com>
2025-03-03 18:03:26 +0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { ThemeProvider } from './context/useTheme'
 import { AuthProvider } from './context/AuthContext'
 import HomePage from './page/user/home'
+import AssistantPage from './page/user/assistant'
 import LoginPage from './page/auth/login'
 import RegisterPage from './page/auth/register'
 import SearchPages from './page/user/search-pages'
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/jobs" element={<SearchPages />} />
             <Route path="/login" element={<LoginPage />} />
+            <Route path="/assistant" element={<AssistantPage />} />
             <Route path="/register" element={<RegisterPage />} />
           </Routes>
         </AuthProvider>
